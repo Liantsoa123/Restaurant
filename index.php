c58f86ea819d09bba15cfcc6bc47251671b83e16
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-06-21 15:32:28 +0300

    create form and make css pretty

--- index.php
+++ index.php
@@ -3,21 +3,7 @@
 
 <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <style type="text/css">
-        html {
-            height: 100%
-        }
-
-        body {
-            height: 100%;
-            margin: 0;
-            padding: 0
-        }
-
-        #carteId {
-            height: 100%
-        }
-    </style>
+    <link rel="stylesheet" href="assets/css/style.css">
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false">
     </script>
     <script src="node_modules/angular/angular.min.js"></script>
@@ -26,8 +12,34 @@
 </head>
 
 <body ng-controller="Map">
-    <div id="carteId" />
+    <div>
+        <form class="formResto">
+            <h3 class="title">Insert Restaurant</h3>
+            <div class="inputStyle" >
+                <label>Nom </label>
+                <input  type="text" name="nom" placeholder="Name restaurant">
+            </div>
+            <div class="inputStyle" >
+                <label>Latitude</label>
+                <input  type="number" name="latitude" placeholder="Latitude">
+            </div>
 
+            <div class="inputStyle" >
+                <label>Longitude</label>
+                <input  type="number" name="longitude" placeholder="Longitude">
+            </div>
+            <div>
+                <label> Image Restaurant </label>
+                <input class="inputFile" type="file" name="img_file">
+            </div>
+
+
+             <button class="btn">Insert</button>
+        </form>
+
+
+    </div>
+    <div id="carteId" />
 </body>
 
 </html>
