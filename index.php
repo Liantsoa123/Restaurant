ca2b724aa96c7c2919a2451a4f9d76528ca7063e
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-06-24 11:29:05 +0300

    add index page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Sig</title>
+</head>
+
+<body>
+    <div>
+        <div>
+            <h1>Restaurant SIG</h1>
+        </div>
+        <div>
+            <a href="front-office.php"> Front Office </a>
+        </div>
+        <div>
+            <a href="back-office.php">Back Office </a>
+        </div>
+    </div>
+</body>
+
+</html>
